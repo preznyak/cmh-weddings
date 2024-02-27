954eed2f0e1b39caaf8b975ef665bce672a6e56c
Laszlo Preznyak <laszlopreznyak11@gmail.com>
2024-02-27 12:02:31 +0100

    First commit

---
+++ src/main/java/hu/preznyak/cmhweddings/CmhWeddingsApplication.java
@@ -0,0 +1,13 @@
+package hu.preznyak.cmhweddings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmhWeddingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmhWeddingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/preznyak/cmhweddings/CmhWeddingsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.preznyak.cmhweddings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmhWeddingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
