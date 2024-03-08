a4a6b28d09c9192e8e5c5a5b27de6e832d268bf8
Laszlo Preznyak <laszlopreznyak11@gmail.com>
2024-03-08 10:11:15 +0100

    Contact entity created

--- src/main/java/hu/preznyak/cmhweddings/domain/Contact.java
+++ src/main/java/hu/preznyak/cmhweddings/domain/Contact.java
@@ -1,2 +1,33 @@
-package hu.preznyak.cmhweddings.domain;public class Contact {
+package hu.preznyak.cmhweddings.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Contact {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 50)
+    private String emailAddress;
+
+    @Column(length = 20)
+    private String phoneNumber;
 }
