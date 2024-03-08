c34abb3f090c8f02eaa5268c8f8cc8cad63f7050
Laszlo Preznyak <laszlopreznyak11@gmail.com>
2024-03-08 10:35:25 +0100

    CMH-32_contact_dto

---
+++ src/main/java/hu/preznyak/cmhweddings/web/model/ContactDto.java
@@ -0,0 +1,32 @@
+package hu.preznyak.cmhweddings.web.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactDto {
+
+    @Null(message = "Id should be null.")
+    private UUID id;
+
+    @Size(min = 5, max = 50, message = "Name length should be between 5 and 50 characters.")
+    @NotBlank
+    private String name;
+
+    @Email(message = "Email should be valid.")
+    private String emailAddress;
+
+    @Size(min = 8, max = 20, message = "Phone number length should be between 8 and 20 characters.")
+    private String phoneNumber;
+}
