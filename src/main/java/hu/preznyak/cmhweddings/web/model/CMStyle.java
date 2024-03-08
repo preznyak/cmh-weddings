badd1aa0da0bff738f1e1c00535d1d88aee44e10
Laszlo Preznyak <laszlopreznyak11@gmail.com>
2024-03-08 12:18:45 +0100

    CMH-36_cmstyle_and_currency

---
+++ src/main/java/hu/preznyak/cmhweddings/web/model/CMStyle.java
@@ -0,0 +1,5 @@
+package hu.preznyak.cmhweddings.web.model;
+
+public enum CMStyle {
+    TRADITIONAL, MODERN, HYBRID
+}
---
+++ src/main/java/hu/preznyak/cmhweddings/web/model/Currency.java
@@ -0,0 +1,5 @@
+package hu.preznyak.cmhweddings.web.model;
+
+public enum Currency {
+    EUR, USD, GBP, HUF, RON
+}
